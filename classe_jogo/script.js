a5672ffb6c00fc0b1c72b363ff20291e501d2d22
Lívia Quirino <liviaquirino2005@gmail.com>
2023-12-16 18:28:57 -0300

    Add files via upload

---
+++ classe_jogo/script.js
@@ -0,0 +1,38 @@
+class Heroi {
+  
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque;
+      switch (this.tipo) {
+        case 'mago':
+          ataque = 'usou magia';
+          break;
+        case 'guerreiro':
+          ataque = 'usou espada';
+          break;
+        case 'monge':
+          ataque = 'usou artes marciais';
+          break;
+        case 'ninja':
+          ataque = 'usou shuriken';
+          break;
+        default:
+          ataque = 'usou um ataque indefinido';
+      }
+  
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  const heroi1 = new Heroi('Aragorn', 35, 'guerreiro');
+  const heroi2 = new Heroi('Gandalf', 60, 'mago');
+  
+  heroi1.atacar(); 
+  heroi2.atacar(); 
+  
+  
